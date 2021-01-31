4cdf0497fd1341135ec70b1c62ca57dd5870779e
mast <78006031+shiju2021@users.noreply.github.com>
2021-01-31 22:20:07 +0800

    Update iboxpayCOOKIE.js

--- Task/iboxpayCOOKIE.js
+++ Task/iboxpayCOOKIE.js
@@ -1,8 +1,8 @@
 
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let iboxpayheaderVal= [process.env.XP_IBOXPAYHEADER]
-let refreshtokenVal= [process.env.XP_REFRESHTOKEN]
+let iboxpayheaderVal= process.env.XP_IBOXPAYHEADER
+let refreshtokenVal= process.env.XP_REFRESHTOKEN
 
 
 
